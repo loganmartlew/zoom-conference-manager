168d5f306d9fc43025bfccf3ba43f35048b29bfe
Logan Martlew <logan.martlew@gmail.com>
2022-04-07 17:13:07 +1200

    Added Netlify deployment to production

--- apps/client/src/App/App.tsx
+++ apps/client/src/App/App.tsx
@@ -3,7 +3,7 @@ import { environment } from '../environments/environment';
 
 const App: FC = () => (
   <div>
-    <h1>Zoom Conference Manager</h1>
+    <h1>Zoom Conference Manager!</h1>
     <p>Platform for managing large scale Zoom events.</p>
     <p>Environment: {environment.mode}</p>
   </div>
