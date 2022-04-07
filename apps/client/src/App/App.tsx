ff4ed9a1f06200d148a0573b4590de39af18eb52
Logan Martlew <logan.martlew@gmail.com>
2022-04-07 15:29:42 +1200

    Added environments for development, staging and production

--- apps/client/src/App/App.tsx
+++ apps/client/src/App/App.tsx
@@ -1,9 +1,11 @@
 import { FC } from 'react';
+import { environment } from '../environments/environment';
 
 const App: FC = () => (
   <div>
     <h1>Zoom Conference Manager</h1>
     <p>Platform for managing large scale Zoom events.</p>
+    <p>Environment: {environment.mode}</p>
   </div>
 );
 
