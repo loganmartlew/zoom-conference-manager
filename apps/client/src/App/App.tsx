a71e0c2af79b90fe8a663c6a84cbb207dacd7041
Logan Martlew <logan.martlew@gmail.com>
2022-05-02 12:47:38 +1200

    Added MUI

--- apps/client/src/App/App.tsx
+++ apps/client/src/App/App.tsx
@@ -1,6 +1,13 @@
 import { FC } from 'react';
+import { CssBaseline } from '@mui/material';
 import AppRoutes from './AppRoutes';
+import './imports.css';
 
-const App: FC = () => <AppRoutes />;
+const App: FC = () => (
+  <>
+    <CssBaseline />
+    <AppRoutes />;
+  </>
+);
 
 export default App;
