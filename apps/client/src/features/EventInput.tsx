552bc13c10edc404a9905c177ce74361fb391630
whatTriangle <49623575+rhysvr@users.noreply.github.com>
2022-05-02 15:29:20 +1200

    added styling, and dynamic date setting

--- apps/client/src/features/EventInput.tsx
+++ apps/client/src/features/EventInput.tsx
@@ -1,19 +1,42 @@
-import { FC, useState, ChangeEvent } from 'react';
-import { Box, TextField } from '@mui/material';
+import { FC, useState, ChangeEvent, MouseEvent } from 'react';
+import { Box, TextField, Button } from '@mui/material';
 
-const style = {
-  marginTop: '1rem',
+const flexSpacing = {
+  margin: '1rem auto',
+  alignSelf: 'center',
+};
+
+const dateStyle = {
+  display: 'flex',
+  justifyContent: 'center',
+  height: '20%',
 };
 
 const EventInput: FC = () => {
+  function getCurrentDate(): string {
+    const date = new Date();
+
+    // append missing 0
+    let month = date.getMonth().toString();
+    if (month.length === 1) {
+      month = `0${month}`;
+    }
+
+    let day = date.getDay().toString();
+    if (day.length === 1) {
+      day = `0${day}`;
+    }
+
+    const year = date.getFullYear().toString();
+    return `${year}-${month}-${day}`;
+  }
+
   const [formData, setFormData] = useState({
     name: '',
     desc: '',
-    start: new Date().toString(),
-    end: new Date().toString(),
+    start: getCurrentDate(),
+    end: getCurrentDate(),
   });
-
-  console.log(formData.start);
 
   function handleChange(event: ChangeEvent<HTMLInputElement>) {
     const { name, value } = event.target;
@@ -25,16 +48,23 @@ const EventInput: FC = () => {
     }));
   }
 
+  function submitEvent(event: MouseEvent<HTMLElement>) {
+    event.preventDefault();
+    // call axios to push of the request
+    // navigate to some other route
+  }
+
   return (
     <Box
       display='flex'
       alignItems='center'
-      justifyContent='space-between'
+      justifyContent='center'
       flexDirection='column'
       height='100vh'
+      width='100%'
     >
       <TextField
-        style={style}
+        style={flexSpacing}
         required
         name='name'
         id='outlined-required'
@@ -45,6 +75,7 @@ const EventInput: FC = () => {
         onChange={handleChange}
       />
       <TextField
+        style={flexSpacing}
         name='desc'
         id='outlined'
         label='Description'
@@ -53,20 +84,52 @@ const EventInput: FC = () => {
         // eslint-disable-next-line react/jsx-no-bind
         onChange={handleChange}
       />
-      <div>
-        <input
-          name='start'
-          value={formData.start}
-          onChange={handleChange}
-          type='date'
-        />
-        <input
-          name='end'
-          value={formData.end}
-          onChange={handleChange}
-          type='date'
-        />
+      <div style={dateStyle}>
+        <div
+          style={{
+            display: 'flex',
+            flexDirection: 'column',
+            alignItems: 'center',
+            margin: 'auto',
+          }}
+        >
+          {/* eslint-disable-next-line jsx-a11y/label-has-associated-control */}
+          <label htmlFor='start'>Start Date</label>
+          <input
+            style={flexSpacing}
+            name='start'
+            value={formData.start}
+            onChange={handleChange}
+            type='date'
+            id='start'
+          />
+        </div>
+        <div style={{ width: '1rem' }} />
+        <div
+          style={{
+            display: 'flex',
+            flexDirection: 'column',
+            alignItems: 'center',
+            margin: 'auto',
+          }}
+        >
+          {/* eslint-disable-next-line jsx-a11y/label-has-associated-control */}
+          <label htmlFor='end'>End Date</label>
+          <input
+            style={flexSpacing}
+            name='end'
+            value={formData.end}
+            onChange={handleChange}
+            type='date'
+            id='end'
+          />
+        </div>
       </div>
+
+      {/* eslint-disable-next-line react/jsx-no-bind */}
+      <Button onClick={submitEvent} variant='outlined'>
+        Create Event
+      </Button>
     </Box>
   );
 };
