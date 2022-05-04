8b988de57729e25e799602230487b2542e877ea3
Logan Martlew <logan.martlew@gmail.com>
2022-05-04 16:44:18 +1200

    Removed singleton datasource

--- apps/api/src/loaders/typeorm.ts
+++ apps/api/src/loaders/typeorm.ts
@@ -3,46 +3,22 @@ import { DataSource } from 'typeorm';
 import { db } from '../config';
 import entities from '../entities';
 
-class AppDataSource {
-  private static instance: AppDataSource;
-
-  private appDataSource: DataSource;
-
-  private constructor() {
-    const dbVars = db();
-
-    this.appDataSource = new DataSource({
-      type: 'postgres',
-      host: dbVars.host,
-      port: dbVars.port,
-      username: dbVars.username,
-      password: dbVars.password,
-      database: dbVars.database,
-      synchronize: true,
-      entities,
-    });
-  }
-
-  public static getInstance() {
-    if (!AppDataSource.instance) {
-      AppDataSource.instance = new AppDataSource();
-    }
-
-    return AppDataSource.instance;
-  }
-
-  public getDataSource() {
-    return this.appDataSource;
-  }
-}
-
-export function getDataSource() {
-  return AppDataSource.getInstance().getDataSource();
-}
-
 export default async () => {
+  const dbVars = db();
+
+  const dataSource = new DataSource({
+    type: 'postgres',
+    host: dbVars.host,
+    port: dbVars.port,
+    username: dbVars.username,
+    password: dbVars.password,
+    database: dbVars.database,
+    synchronize: true,
+    entities,
+  });
+
   try {
-    await getDataSource().initialize();
+    await dataSource.initialize();
     console.info('Connected to database');
   } catch (error) {
     console.error(error);
