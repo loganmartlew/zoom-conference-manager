4e7b4b8fa06c3ee8991dc3ddf87c852ad1377b8f
Logan Martlew <logan.martlew@gmail.com>
2022-05-08 14:17:23 +1200

    Moved providers of App to a Providers component

--- apps/client/src/App/App.tsx
+++ apps/client/src/App/App.tsx
@@ -1,21 +1,14 @@
 import { FC } from 'react';
-import { QueryClient, QueryClientProvider } from 'react-query';
 import { CssBaseline } from '@mui/material';
-import { LocalizationProvider } from '@mui/x-date-pickers';
-import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
-import locale from 'dayjs/locale/en-nz';
 import AppRoutes from './AppRoutes';
 import './imports.css';
-
-const queryClient = new QueryClient();
+import Providers from './Providers';
 
 const App: FC = () => (
-  <QueryClientProvider client={queryClient}>
-    <LocalizationProvider dateAdapter={AdapterDayjs} locale={locale}>
-      <CssBaseline />
-      <AppRoutes />
-    </LocalizationProvider>
-  </QueryClientProvider>
+  <Providers>
+    <CssBaseline />
+    <AppRoutes />
+  </Providers>
 );
 
 export default App;
