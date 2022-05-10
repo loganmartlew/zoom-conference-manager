6c42b7556a74898f267acc57f42e4cef889d7403
rhysvr <rhys1964@gmail.com>
2022-05-10 13:30:29 +1200

    adding error msg loading, and info msg

--- apps/client/src/features/Events/EventsList.tsx
+++ apps/client/src/features/Events/EventsList.tsx
@@ -1,4 +1,5 @@
 import { FC } from 'react';
+import { CircularProgress, Alert, AlertTitle } from '@mui/material';
 import { useAllEvents } from './api/getEvents';
 import EventCard from './EventCard';
 
@@ -6,15 +7,24 @@ const EventsList: FC = () => {
   const { data: events, isLoading, isError } = useAllEvents();
 
   if (isError) {
-    return <p>An error occurred</p>;
+    return (
+      <Alert onClose={() => {}} severity='error' variant='outlined'>
+        <AlertTitle>Error</AlertTitle>An error occurred
+      </Alert>
+    );
   }
 
   if (isLoading) {
-    return <p>Loading...</p>;
+    return <CircularProgress color='secondary' />;
   }
 
   if (events && events?.length < 1) {
-    return <p>No Events</p>;
+    return (
+      <Alert onClose={() => {}} severity='success' variant='outlined'>
+        <AlertTitle>No Events</AlertTitle>
+        No events found...
+      </Alert>
+    );
   }
 
   return (
