c3c81e026ae7e0318053a08d745534e0a440486a
Logan Martlew <logan.martlew@gmail.com>
2022-05-22 22:30:48 +1200

    Prettier

--- apps/client/src/pages/NewMeetingPage.tsx
+++ apps/client/src/pages/NewMeetingPage.tsx
@@ -3,15 +3,15 @@ import { Box, Typography } from '@mui/material';
 import MeetingInput from '../features/Meetings/MeetingForm';
 
 const NewMeetingPage: FC = () => {
-    return  (
-        <Box sx={{ display: 'grid', placeContent: 'center', minHeight: '100vh' }}>
-            <Typography variant='h3' sx={{ mb: 3 }}>
-                New Meeting
-            </Typography>
-            <MeetingInput />
-        <Box sx={{ mb: 15 }} />
+  return (
+    <Box sx={{ display: 'grid', placeContent: 'center', minHeight: '100vh' }}>
+      <Typography variant='h3' sx={{ mb: 3 }}>
+        New Meeting
+      </Typography>
+      <MeetingInput />
+      <Box sx={{ mb: 15 }} />
     </Box>
-    );
+  );
 };
 
 export default NewMeetingPage;
