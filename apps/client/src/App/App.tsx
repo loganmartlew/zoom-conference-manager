1ad8959e89a1cd588ed0ccbfd8fe05e5c919e2f4
Logan Martlew <logan.martlew@gmail.com>
2022-05-23 12:48:43 +1200

    Added react toastify and set colors to mui theme

--- apps/client/src/App/App.tsx
+++ apps/client/src/App/App.tsx
@@ -1,15 +1,20 @@
 import { FC } from 'react';
 import { CssBaseline } from '@mui/material';
+import { ToastContainer } from 'react-toastify';
 import globalStyles from './globalStyles';
 import AppRoutes from './AppRoutes';
-import './imports.css';
 import Providers from './Providers';
+import notificationStyles from './notificationStyles';
+import './imports.css';
+import 'react-toastify/dist/ReactToastify.css';
 
 const App: FC = () => (
   <Providers>
     <CssBaseline />
     {globalStyles}
     <AppRoutes />
+    <ToastContainer />
+    {notificationStyles}
   </Providers>
 );
 
