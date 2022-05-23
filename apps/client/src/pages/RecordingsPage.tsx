3a130e672ac997f77a60c9e0b643760887e34afe
amylouiseee <ameh456@gmail.com>
2022-05-23 19:05:58 +1200

    changed recordings page to be more readable

--- apps/client/src/pages/RecordingsPage.tsx
+++ apps/client/src/pages/RecordingsPage.tsx
@@ -1,16 +1,12 @@
 import { FC } from 'react';
-import { Box, Typography } from '@mui/material';
 import EventsList from '../features/Events/EventsList';
 
 const RecordingsPage: FC = () => {
   return (
-    <Box sx={{ display: 'grid', placeContent: 'center', minHeight: '100vh' }}>
-      <Typography variant='h3' sx={{ mb: 3 }}>
-        Recent Events
-        <EventsList />
-      </Typography>
-      <Box sx={{ mb: 15 }} />
-    </Box>
+    <>
+      <h1>Recent Events</h1>
+      <EventsList />
+    </>
   );
 };
 
