baf912f940f9c708942fefa8f04d7a016b594f46
Logan Martlew <logan.martlew@gmail.com>
2022-06-14 15:12:10 +1200

    Changed layout of event card

--- apps/client/src/features/Events/EventCard.tsx
+++ apps/client/src/features/Events/EventCard.tsx
@@ -1,5 +1,5 @@
 import { FC } from 'react';
-import { Paper, Button, Stack, Typography, styled } from '@mui/material';
+import { Paper, Stack, Typography, styled } from '@mui/material';
 
 interface EventCardProps {
   name: string;
@@ -8,46 +8,28 @@ interface EventCardProps {
   end: string;
 }
 
-const InfoStack = styled(Stack)(() => ({
-  gap: '1rem',
-  textOverflow: 'ellipsis',
-  overflow: 'hidden',
-}));
-
-const GenericTypography = styled(Typography)(() => ({
-  overflow: 'hidden',
-  textOverflow: 'ellipsis',
-}));
-
 const EventCard: FC<EventCardProps> = (props) => {
   const { name, desc, start, end } = props;
   return (
     <Paper
       sx={{
-        width: '85%',
-        padding: '0.8rem',
-        marginTop: '1rem',
-        display: 'block',
-        textAlign: 'unset',
+        padding: '1.5em',
+        maxWidth: '100ch',
       }}
-      component={Button}
       elevation={3}
     >
       <Stack direction='row' justifyContent='space-between'>
-        <InfoStack width='90%' direction='column' justifyContent='space-evenly'>
-          <GenericTypography variant='h5'>{name}</GenericTypography>
-          <GenericTypography variant='body2' width='50%'>
-            {desc}
-          </GenericTypography>
-        </InfoStack>
-        <InfoStack width='10%' direction='column' justifyContent='space-evenly'>
-          <GenericTypography alignSelf='end' variant='body2'>
-            {start}
-          </GenericTypography>
-          <GenericTypography alignSelf='end' variant='body2'>
-            {end}
-          </GenericTypography>
-        </InfoStack>
+        <Stack spacing={1}>
+          <Stack direction='row' alignItems='center' flexWrap='wrap'>
+            <Typography variant='h5' fontWeight={500} mr={3}>
+              {name}
+            </Typography>
+            <Typography
+              sx={{ width: 'max-content', whiteSpace: 'noWrap' }}
+            >{`${start} - ${end}`}</Typography>
+          </Stack>
+          <Typography variant='body1'>{desc}</Typography>
+        </Stack>
       </Stack>
     </Paper>
   );
