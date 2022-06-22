14db36e3ae21bbc3ab73e5608c44689dfb78cf67
Logan Martlew <logan.martlew@gmail.com>
2022-06-22 22:04:36 +1200

    Added details to event details page

--- apps/client/src/features/Events/EventDetails.tsx
+++ apps/client/src/features/Events/EventDetails.tsx
@@ -1,6 +1,8 @@
 import { FC } from 'react';
+import { Link } from 'react-router-dom';
 import { IEvent } from '@zoom-conference-manager/types';
-import { Stack, Typography } from '@mui/material';
+import { Stack, Typography, Button } from '@mui/material';
+import { Add, Upload } from '@mui/icons-material';
 import EventStatusBadge from './EventStatusBadge';
 
 interface Props {
@@ -19,13 +21,49 @@ const EventDetails: FC<Props> = ({ event, isLoading }) => {
 
   return (
     <Stack spacing={2}>
-      <Stack direction='row' spacing={1} alignItems='center'>
-        <EventStatusBadge status='draft' />
-        <Typography variant='body2'>
-          Event has not yet been published
-        </Typography>
+      <Stack spacing={1}>
+        <Typography variant='h5'>Status</Typography>
+        <Stack direction='row' spacing={1} alignItems='center'>
+          <EventStatusBadge status='draft' />
+          <Typography variant='body2'>
+            Event has not yet been published
+          </Typography>
+        </Stack>
+        <Button variant='contained' size='small' sx={{ width: 'max-content' }}>
+          Publish
+        </Button>
       </Stack>
-      <Typography>{event.description}</Typography>
+
+      <Stack spacing={1}>
+        <Typography variant='h5'>Dates</Typography>
+        <Typography>{`${event.startDate} - ${event.endDate}`}</Typography>
+        <Button variant='outlined' size='small' sx={{ width: 'max-content' }}>
+          View On Calendar
+        </Button>
+      </Stack>
+
+      <Stack spacing={1}>
+        <Typography variant='h5'>Description</Typography>
+        <Typography>{event.description}</Typography>
+      </Stack>
+
+      <Stack spacing={1}>
+        <Typography variant='h5'>Meetings</Typography>
+        <Stack direction='row' spacing={1}>
+          <Button
+            component={Link}
+            to={`/new-event?event=${event.id}`}
+            variant='contained'
+            size='small'
+            startIcon={<Add />}
+          >
+            Add Meeting
+          </Button>
+          <Button variant='outlined' size='small' startIcon={<Upload />}>
+            Upload Meetings
+          </Button>
+        </Stack>
+      </Stack>
     </Stack>
   );
 };
