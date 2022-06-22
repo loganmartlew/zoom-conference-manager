172ad7116245451d11491989c4a41388a7ad1c47
Logan Martlew <logan.martlew@gmail.com>
2022-06-22 20:58:04 +1200

    Added new event button to events page

--- apps/client/src/pages/EventsPage.tsx
+++ apps/client/src/pages/EventsPage.tsx
@@ -1,13 +1,23 @@
 import { FC } from 'react';
-import { Typography } from '@mui/material';
+import { Link } from 'react-router-dom';
+import { Stack, Typography, Button } from '@mui/material';
+import { Add } from '@mui/icons-material';
 import EventsList from '../features/Events/EventsList';
 
 const EventsPage: FC = () => {
   return (
     <>
-      <Typography variant='h3' sx={{ mb: 3 }}>
-        Events
-      </Typography>
+      <Stack direction='row' spacing={3} alignItems='center' sx={{ mb: 3 }}>
+        <Typography variant='h3'>Events</Typography>
+        <Button
+          component={Link}
+          to='/new-event'
+          variant='contained'
+          startIcon={<Add />}
+        >
+          New Event
+        </Button>
+      </Stack>
       <EventsList />
     </>
   );
