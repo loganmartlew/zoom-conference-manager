c0db056ea3fadfc1c31753c3709453d4c4eb91b5
Logan Martlew <logan.martlew@gmail.com>
2022-06-22 21:08:10 +1200

    Added page for event details

---
+++ apps/client/src/pages/EventDetailsPage.tsx
@@ -0,0 +1,14 @@
+import { FC } from 'react';
+import { Typography } from '@mui/material';
+
+const EventDetailsPage: FC = () => {
+  return (
+    <>
+      <Typography variant='h3' sx={{ mb: 3 }}>
+        Event Details
+      </Typography>
+    </>
+  );
+};
+
+export default EventDetailsPage;
