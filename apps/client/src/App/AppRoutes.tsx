f9fe3982bec537fe7f8d09041c98864af7e2423a
rhysvr <rhys1964@gmail.com>
2022-06-23 13:53:06 +1200

    added basic setup

--- apps/client/src/App/AppRoutes.tsx
+++ apps/client/src/App/AppRoutes.tsx
@@ -2,22 +2,24 @@ import { FC } from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import NavLayout from '../features/Navigation/NavLayout';
 
-import EventsPage from '../pages/EventsPage';
-import EventDetailsPage from '../pages/EventDetailsPage';
-import NewEventPage from '../pages/NewEventPage';
-import NewMeetingPage from '../pages/NewMeetingPage';
+// import EventsPage from '../pages/EventsPage';
+// import EventDetailsPage from '../pages/EventDetailsPage';
+// import NewEventPage from '../pages/NewEventPage';
+// import NewMeetingPage from '../pages/NewMeetingPage';
+import UpdateMeeting from '../features/Meetings/UpdateMeeting';
 
 const AppRoutes: FC = () => {
   return (
     <Router>
       <Routes>
         <Route element={<NavLayout />}>
-          <Route path='/' element={<h1>Home</h1>} />
+          <Route path='/' element={<UpdateMeeting />} />
+          {/* <Route path='/' element={<h1>Home</h1>} />
           <Route path='/dashboard' element={<h1>Dashboard</h1>} />
           <Route path='/events' element={<EventsPage />} />
           <Route path='/events/:id' element={<EventDetailsPage />} />
           <Route path='/new-event' element={<NewEventPage />} />
-          <Route path='/new-meeting' element={<NewMeetingPage />} />
+          <Route path='/new-meeting' element={<NewMeetingPage />} /> */}
         </Route>
       </Routes>
     </Router>
---
+++ apps/client/src/features/Meetings/UpdateMeeting.tsx
@@ -0,0 +1,17 @@
+import { FC } from 'react';
+
+// interface Props {
+//   getMeeting: () => void;
+//   meetingID: number;
+// }
+
+const UpdateMeeting: FC = () => {
+  return (
+    <div>
+      <div>cool</div>
+      <div>beans</div>
+    </div>
+  );
+};
+
+export default UpdateMeeting;
