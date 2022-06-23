65890674088f1c9f2723acb1e7260f5d21bbfd1b
Logan Martlew <logan.martlew@gmail.com>
2022-06-23 17:23:33 +1200

    Removed unused user routes/controller

--- apps/api/src/routes/index.ts
+++ apps/api/src/routes/index.ts
@@ -2,13 +2,11 @@ import express from 'express';
 
 import meetingRoute from './meeting';
 import eventRoute from './event';
-import userRoute from './user';
 
 const router = express.Router();
 
 /// All routes here :
 router.use('/meeting', meetingRoute);
 router.use('/event', eventRoute);
-router.use('/user', userRoute);
 
 export default router;
