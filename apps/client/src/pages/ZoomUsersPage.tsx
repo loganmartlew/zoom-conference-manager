d9bab3a26fd7f95c1c5abe95a34209c2df18997b
Logan Martlew <logan.martlew@gmail.com>
2022-07-14 16:16:55 +1200

    Added dialog to add user

--- apps/client/src/pages/ZoomUsersPage.tsx
+++ apps/client/src/pages/ZoomUsersPage.tsx
@@ -1,16 +1,35 @@
-import { FC } from 'react';
-import { Box, Typography } from '@mui/material';
+import { FC, useState } from 'react';
+import { Box, Typography, Stack, Button } from '@mui/material';
+import { PersonAdd } from '@mui/icons-material';
 import ZoomUserList from '../features/ZoomUsers/ZoomUserList';
 import { useAllZoomUsers } from '../features/ZoomUsers/api/getZoomUsers';
+import AddUserDialog from '../features/ZoomUsers/AddUserDialog';
 
 const ZoomUsersPage: FC = () => {
+  const [dialogOpen, setDialogOpen] = useState(false);
+
   const { data, isLoading } = useAllZoomUsers();
 
   return (
     <Box sx={{ display: 'grid', placeContent: 'center' }}>
-      <Typography variant='h3' sx={{ mb: 1 }}>
-        Zoom Users
-      </Typography>
+      <Stack direction='row' spacing={2} alignItems='center'>
+        <Typography variant='h3' sx={{ mb: 1 }}>
+          Zoom Users
+        </Typography>
+        <Button
+          variant='contained'
+          startIcon={<PersonAdd />}
+          sx={{ height: 'max-content' }}
+          onClick={() => setDialogOpen(true)}
+        >
+          Add User
+        </Button>
+        <AddUserDialog
+          open={dialogOpen}
+          handleClose={() => setDialogOpen(false)}
+          onConfirm={() => {}}
+        />
+      </Stack>
       <Typography sx={{ mb: 3 }}>
         Users in this list will be used to schedule meetings by this app
       </Typography>
