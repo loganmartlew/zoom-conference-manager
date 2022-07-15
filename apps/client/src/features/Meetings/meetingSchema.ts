290c799dd1909984752f965de6bbef7c0774fd7c
amylouiseee <ameh456@gmail.com>
2022-07-15 22:11:59 +1200

    added 24hr time

--- apps/client/src/features/Meetings/meetingSchema.ts
+++ apps/client/src/features/Meetings/meetingSchema.ts
@@ -4,14 +4,17 @@ const meetingSchema = object({
   ubid: string().required('UBID is required.'),
   name: string().required('Name is required.'),
   startDate: date()
-    .nullable()
     .typeError('Invalid date.')
     .required('Start date is required.'),
+  startTime: string()
+    .required('Start time is required.')
+    .length(5, 'Must be in format XX:XX.')
+    .matches(/[0-9]{2}:[0-9]{2}/, 'Must be in format XX:XX.'),
   duration: number()
     .required('Meeting duration is required.')
     .positive('Number must be positive.')
     .min(1, 'Meeting duration cannot be less than 1 minute.'),
-  eventId: string().required('Must select Event.'),
+  eventId: string().required('Must select an Event.'),
 });
 
 export default meetingSchema;
