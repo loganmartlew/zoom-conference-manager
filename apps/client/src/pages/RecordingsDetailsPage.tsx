2c15cfda5683dc5c37d512c02479e34e2f86af24
amylouiseee <ameh456@gmail.com>
2022-08-02 14:52:36 +1200

    get all recordings

--- apps/client/src/pages/RecordingsDetailsPage.tsx
+++ apps/client/src/pages/RecordingsDetailsPage.tsx
@@ -1,4 +1,4 @@
-import { Typography } from '@mui/material';
+import { Typography, Button, Stack } from '@mui/material';
 import { FC } from 'react';
 import { useParams } from 'react-router-dom';
 import { useEvent } from '../features/Events/api/getEvent';
@@ -10,14 +10,25 @@ const RecordingsDetailsPage: FC = () => {
 
   return (
     <>
-      <Typography
-        variant='h3'
-        sx={{
-          mb: 3,
-        }}
-      >
-        {data?.name || 'Event Details'}
-      </Typography>
+      <Stack direction='row' spacing={7}>
+        <Typography
+          variant='h3'
+          sx={{
+            mb: 3,
+          }}
+        >
+          {data?.name || 'Event Details'}
+        </Typography>
+        <Button
+          variant='contained'
+          sx={{
+            width: 'max-content',
+            height: 'max-content',
+          }}
+        >
+          Get All Recordings
+        </Button>
+      </Stack>
       <RecordingsDetails event={data} isLoading={isLoading} />
     </>
   );
