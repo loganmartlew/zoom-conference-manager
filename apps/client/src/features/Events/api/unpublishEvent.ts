b2a07796fb078eb618e0531e3f1b6cade794efd4
Logan Martlew <logan.martlew@gmail.com>
2022-08-03 09:58:16 +1200

    Added mutation to unpublish from client

---
+++ apps/client/src/features/Events/api/unpublishEvent.ts
@@ -0,0 +1,28 @@
+import { UnpublishEvent } from '@zoom-conference-manager/api-interfaces';
+import { useMutation, useQueryClient } from 'react-query';
+import { axios } from '../../../config/axios';
+import { eventKey } from './getEvent';
+import fetchFromApi from '../../../util/fetchFromApi';
+
+export const publishEvent = async (eventId: string) => {
+  return fetchFromApi<UnpublishEvent>(
+    axios.patch(`/events/${eventId}/unpublish`)
+  );
+};
+
+export const usePublishEvent = (
+  onSuccess: () => void,
+  onError: (error: unknown, variables: string) => void
+) => {
+  const queryClient = useQueryClient();
+
+  const { mutate, isLoading } = useMutation(publishEvent, {
+    onSuccess,
+    onError,
+    onSettled: (_, __, id) => {
+      queryClient.invalidateQueries([...eventKey, id]);
+    },
+  });
+
+  return { mutate, isLoading };
+};
