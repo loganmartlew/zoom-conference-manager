da43073fc1826fe2b34a9862b32b4593dd2bedf1
Logan Martlew <logan.martlew@gmail.com>
2022-08-02 12:41:28 +1200

    Added publishing logic into Zoom service

--- apps/api/src/services/ZoomService.ts
+++ apps/api/src/services/ZoomService.ts
@@ -3,36 +3,47 @@ import Event from '../entities/Event';
 import Meeting from '../entities/Meeting';
 import { axios } from '../loaders/axios';
 import { Logger } from '../loaders/logger';
-import { assignUserMeetingBlocks } from '../util/publish/assignUserMeetingBlocks';
-import { formatUserMeetings } from '../util/publish/formatUserMeetings';
+import { assignMeetings } from '../util/publish/assignMeetings';
 import MeetingService from './MeetingService';
+import ZoomUserService from './ZoomUserService';
 
 export default class ZoomService {
-  static async assignMeetings(meetings: Meeting[]) {
-    const users: string[] = ['logan', 'rhys', 'aman', 'amy', 'jack'];
-
-    const userMeetingBlocks = assignUserMeetingBlocks(users, meetings);
-
-    const userMeetings = formatUserMeetings(userMeetingBlocks);
-
-    return userMeetings;
-  }
-
   static async publishEvent(event: Event) {
-    const assignedMeetings: { user: string; meeting: Meeting }[] =
-      await ZoomService.assignMeetings(event.meetings);
+    const users = await ZoomUserService.getAll();
+
+    const [userMeetings, unassignedMeetings] = assignMeetings(
+      users,
+      event.meetings
+    );
+
+    if (unassignedMeetings.length > 0) {
+      Logger.error(`Unable to assign ${unassignedMeetings.length} meetings`);
+      return;
+    }
 
     Logger.info(`Assigned meetings`);
-    Logger.info(JSON.stringify(assignedMeetings, null, 2));
+    Logger.info(JSON.stringify(userMeetings, null, 2));
+
+    await Promise.all(
+      userMeetings.map(async (userMeeting) => {
+        await Promise.all(
+          userMeeting.meetings.map(async (meeting) => {
+            await ZoomService.scheduleMeeting(meeting, userMeeting.email);
+          })
+        );
+      })
+    );
   }
 
   static async scheduleMeeting(meeting: Meeting, userEmail: string) {
+    const SCHEDULED_MEETING = 2;
+
     const meetingData = {
       topic: meeting.name,
       schedule_for: userEmail,
       start_time: meeting.startDateTime.toISOString(),
-      duration: meeting.duration,
-      type: 2,
+      duration: meeting.duration + 30,
+      type: SCHEDULED_MEETING,
     };
 
     const res = await axios.post<{ id: number }>(
