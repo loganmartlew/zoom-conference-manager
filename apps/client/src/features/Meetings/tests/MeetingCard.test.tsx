0d626e26dce72f6725557b617f964f83566dc8d8
amylouiseee <ameh456@gmail.com>
2022-08-23 15:37:04 +1200

    meeting card test

--- apps/client/src/features/Meetings/tests/MeetingCard.test.tsx
+++ apps/client/src/features/Meetings/tests/MeetingCard.test.tsx
@@ -2,7 +2,7 @@ import dayjs from 'dayjs';
 import { renderWithRouter, screen } from '../../../test-utils';
 import MeetingCard from '../MeetingCard';
 
-describe('Test RecordingCard', () => {
+describe('Test MeetingCard', () => {
   test('Card can be rendered', async () => {
     renderWithRouter(
       <MeetingCard
@@ -10,7 +10,7 @@ describe('Test RecordingCard', () => {
           ubid: '1',
           name: 'test',
           startDateTime: dayjs('2022-08-22 12:00').toDate(),
-          endDateTime: dayjs('2022-08-22').toDate(),
+          endDateTime: dayjs('2022-08-22 12:30').toDate(),
           zoomId: '1',
         }}
       />,
@@ -18,7 +18,13 @@ describe('Test RecordingCard', () => {
     );
 
     const cardName = screen.getByText(/test/i);
+    const cardDate = screen.getByText(/2022-08-22/i);
+    const cardStart = screen.getByText(/1200/i);
+    const cardEnd = screen.getByText(/1230/i);
 
     expect(cardName).toBeTruthy();
+    expect(cardDate).toBeTruthy();
+    expect(cardStart).toBeTruthy();
+    expect(cardEnd).toBeTruthy();
   });
 });
