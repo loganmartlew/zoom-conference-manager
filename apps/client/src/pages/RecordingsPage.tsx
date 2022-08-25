c0f516952b5d886be056935dc6431bcdd490c485
amylouiseee <ameh456@gmail.com>
2022-08-25 16:17:46 +1200

    fixed heading for recordings

--- apps/client/src/pages/RecordingsPage.tsx
+++ apps/client/src/pages/RecordingsPage.tsx
@@ -1,10 +1,13 @@
 import { FC } from 'react';
+import { Typography } from '@mui/material';
 import RecordingsList from '../features/Recordings/RecordingsList';
 
 const RecordingsPage: FC = () => {
   return (
     <>
-      <h1>Recent Events</h1>
+      <Typography variant='h3' sx={{ mb: 3 }}>
+        Recent Events
+      </Typography>
       <RecordingsList />
     </>
   );
