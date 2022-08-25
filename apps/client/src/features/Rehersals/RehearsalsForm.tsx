00f6e5a8f9875f7333913fa99d14abc2ab35b0c0
amylouiseee <ameh456@gmail.com>
2022-08-26 10:35:40 +1200

    set up form and other files

---
+++ apps/client/src/features/Rehersals/RehearsalsForm.tsx
@@ -0,0 +1,7 @@
+import { FC } from 'react';
+
+const RehearsalsForm: FC = () => {
+  return <h1>Placeholder</h1>;
+};
+
+export default RehearsalsForm;
--- apps/client/src/pages/NewRehearsalsPage.tsx
+++ apps/client/src/pages/NewRehearsalsPage.tsx
@@ -1,11 +1,15 @@
 import { FC } from 'react';
 import { Typography } from '@mui/material';
+import RehearsalsForm from '../features/Rehersals/RehearsalsForm';
 
 const NewRehearsalsPage: FC = () => {
   return (
-    <Typography variant='h3' sx={{ mb: 3 }}>
-      New Rehearsal
-    </Typography>
+    <>
+      <Typography variant='h3' sx={{ mb: 3 }}>
+        New Rehearsal
+      </Typography>
+      <RehearsalsForm />
+    </>
   );
 };
 
