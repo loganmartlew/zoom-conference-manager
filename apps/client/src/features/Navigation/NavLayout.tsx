3a66f70b6c49a35f7c933bf5af897547df69768e
amylouiseee <ameh456@gmail.com>
2022-08-25 16:27:50 +1200

    spelling again

--- apps/client/src/features/Navigation/NavLayout.tsx
+++ apps/client/src/features/Navigation/NavLayout.tsx
@@ -22,7 +22,7 @@ const items: NavItem[] = [
   { text: 'Events', path: '/events', icon: <Event /> },
   { text: 'New Event', path: '/new-event', icon: <AddBox /> },
   { text: 'New Meeting', path: '/new-meeting', icon: <AddBox /> },
-  { text: 'Rehersals', path: '/rehersals', icon: <CoPresent /> },
+  { text: 'Rehearsals', path: '/rehearsals', icon: <CoPresent /> },
   { text: 'Recordings', path: '/recordings', icon: <VideoFile /> },
   { text: 'Add Account', path: '/add-account', icon: <PersonAdd /> },
   { text: 'Zoom Users', path: '/zoom-users', icon: <People /> },
