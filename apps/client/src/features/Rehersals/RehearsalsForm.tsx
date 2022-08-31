5cfbd04dc240c47534ae2b2c332293242dc813df
amylouiseee <ameh456@gmail.com>
2022-08-31 14:28:57 +1200

    form setup and components

--- apps/client/src/features/Rehersals/RehearsalsForm.tsx
+++ apps/client/src/features/Rehersals/RehearsalsForm.tsx
@@ -1,10 +1,19 @@
 import { FC } from 'react';
 import { styled, Stack } from '@mui/material';
+import { FieldError } from 'react-hook-form';
 import TextInput from '../../components/forms/TextInput';
+import { useRehearsalsForm } from './useRehearsalsForm';
+import DatePicker from '../../components/forms/DatePicker';
 
 const Form = styled('form')({});
 
 const RehearsalsForm: FC = () => {
+  const {
+    control,
+    // handleSubmit,
+    formState: { errors },
+  } = useRehearsalsForm();
+
   return (
     <Form
       autoComplete='off'
@@ -16,7 +25,19 @@ const RehearsalsForm: FC = () => {
       }}
     >
       <Stack spacing={2}>
-        <TextInput name='name' label='Name' autoFocus />
+        <TextInput
+          name='name'
+          label='Name'
+          control={control}
+          error={errors.name}
+          autoFocus
+        />
+        <DatePicker
+          name='startDate'
+          label='Date'
+          control={control}
+          error={errors.startDate as FieldError | void}
+        />
       </Stack>
     </Form>
   );
--- apps/client/src/features/Rehersals/useRehearsalsForm.ts
+++ apps/client/src/features/Rehersals/useRehearsalsForm.ts
@@ -1,10 +1,31 @@
 import { yupResolver } from '@hookform/resolvers/yup';
+import dayjs from 'dayjs';
+import { useForm } from 'react-hook-form';
+import rehearsalSchema from './rehearsalSchema';
 
 export interface IFormInput {
   name: string;
-  startDateTime: Date;
-  endDateTime: Date;
+  startDate: Date;
+  startTime: Date;
+  endTime: Date;
   presenter: string;
   presEmail: string;
   rehearsalId: string;
 }
+
+export const useRehearsalsForm = () => {
+  const form = useForm<IFormInput>({
+    defaultValues: {
+      name: '',
+      startDate: dayjs().toDate(),
+      startTime: new Date(0, 0, 0, 0, 0),
+      endTime: new Date(0, 0, 0, 0, 0),
+      presenter: '',
+      presEmail: '',
+      rehearsalId: '',
+    },
+    resolver: yupResolver(rehearsalSchema),
+  });
+
+  return { ...form };
+};
