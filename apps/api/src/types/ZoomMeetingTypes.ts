61e7bc65dad10b6c106cc7760e2182519ad8de04
Jackk-Doe <dodo-stv@hotmail.com>
2022-09-23 16:04:55 +1200

    Added ZoomMeetingType enum

---
+++ apps/api/src/types/ZoomMeetingTypes.ts
@@ -0,0 +1,8 @@
+// For using in Query-string parameter
+export enum ZoomMeetingType {
+  SCHEDULED = 'scheduled',
+  LIVE = 'live',
+  UPCOMING = 'upcoming',
+  UPCOMING_MEETINGS = 'upcoming_meetings',
+  PREVIOUS_MEETINGS = 'upcoming_meetings',
+}
