004b0a6492ae8e0a4f852668e2b4548d08bcd652
Aman Perhar <amanperhar10@gmail.com>
2022-10-30 22:59:52 +1300

    Fixed cell opening on click

--- apps/client/src/pages/CalendarPage.tsx
+++ apps/client/src/pages/CalendarPage.tsx
@@ -3,7 +3,7 @@ import { FC, useEffect, useState } from 'react';
 import dayjs from 'dayjs';
 import { MenuItem, Select, SelectChangeEvent, Tab, Tabs } from '@mui/material';
 import { useAllEvents } from '../features/Events/api/getEvents';
-import Calendar from '../features/calendar/Calendar';
+import Calendar from '../features/Calendar/Calendar';
 
 const CalendarPage: FC = () => {
   const [view, setView] = useState<'events' | 'meetings'>('events');
